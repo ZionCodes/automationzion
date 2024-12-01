4c69185b23378e5426b247aee0dee75b00d1be14
Zion <ziongonet@gmail.com>
2024-12-01 09:34:50 +0100

    animation

--- src/lib/Tools.svelte
+++ src/lib/Tools.svelte
@@ -7,7 +7,7 @@
 <div class="grid grid-cols-2 gap-4 w-full">
     {#each data as tools}
     <a href="/" in:fly={{ y:100, duration:500, opacity:0 }} class="hover:scale-105">
-        <div class="flex flex-col space-y-1 rounded-lg hover:bg-gray-100 bg-white py-0.5 pl-0.5 border border-gray-200" style="opacity: 1; transform: none;">
+        <div class="flex flex-col space-y-1 rounded-lg hover:bg-gray-100 bg-white py-0.5 pl-0.5 border border-gray-200" style="opacity: 1; transform: none;" in:fly={{ y:100, duration:500, opacity:0 }}>
             <div class="w-full flex flex-row items-center">
                 <img src={`https://call-until.pockethost.io/api/files/tools/${tools.id}/${tools.image}`} alt="" loading="eager" width="full" height="full" class='object-fit w-[50px] h-[50px] mr-4 rounded-lg' style="color:transparent">
                 <div>
